de5ab5d1edb50fb6ab020bfd86b5dbd077ee4b98
= <=>
2021-06-17 11:47:20 -0300

    Constrindo uma aplicação que leia o salário de um funcionário, a taxa de aumento e calcule o salário reajustado

---
+++ reajusteSalario/reajusteSalario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace reajusteSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 0, taxa = 0, novoSalario = 0;
+            string nome;
+            do
+            {
+                Console.WriteLine("Digite o seu nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite o valor do seu salário atual: ");
+                string entradaSalario = Console.ReadLine();
+                salario = Convert.ToDouble(entradaSalario);
+                Console.WriteLine("Digite o percentual do aumento: ");
+                string entradaTaxa = Console.ReadLine();
+                taxa = Convert.ToDouble(entradaTaxa);
+
+                novoSalario = salario + (salario * (taxa / 100));
+
+                Console.WriteLine("Olá " + nome + ", o seu novo salário é: R$" + Math.Round(novoSalario, 2));
+            } while (nome != string.Empty);
+        }
+    }
+}
